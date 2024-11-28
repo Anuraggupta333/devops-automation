7e0872d3a640c609f48702b895a14aa19dbd0210
Anurag Gupta <anuraggangil@gmail.com>
2024-11-28 22:49:58 +0530

    initial commit

---
+++ src/test/java/com/example/Devops_integeration/DevopsIntegerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Devops_integeration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsIntegerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
